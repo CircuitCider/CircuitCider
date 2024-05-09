e8f2f3af4e7cc390ffd6dcf0e291fad60a7de61a
rydb <ryan.dobe@gmail.com>
2024-05-09 17:11:45 -0500

    added changes to combat crate

--- crates/combat/examples/testing_world.rs
+++ crates/combat/examples/testing_world.rs
@@ -1,99 +1,56 @@
-use app_core::{plugins::AppSourcesPlugin, ExecLocation, ROOT};
-use bevy::prelude::*;
-use bevy_obj::ObjPlugin;
-use bevy_rapier3d::{
-    plugin::{NoUserData, RapierPhysicsPlugin},
-    render::RapierDebugRenderPlugin,
-};
-use bevy_serialization_extras::prelude::{
-    AssetSpawnRequest, AssetSpawnRequestQueue, PhysicsBundle, PhysicsSerializationPlugin,
-    SerializationPlugin,
-};
-use bevy_serialization_urdf::{
-    loaders::urdf_loader::Urdf,
-    plugin::{AssetSourcesUrdfPlugin, UrdfSerializationPlugin},
-};
-use bevy_ui_extras::systems::{visualize_right_sidepanel_for, visualize_window_for};
-use robot_editor::{
-    plugins::RobotEditorPlugin, raycast_utils::debug::debug_mouse_info, states::RobotEditorState,
-};
 
-pub fn main() {
+//! A simple 3D scene with light shining over a cube sitting on a plane.
+
+use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use combat::{components::Health, ui::health_ui};
+
+fn main() {
     App::new()
-        // app sources
-        .add_plugins(AppSourcesPlugin::CRATE)
-        .add_plugins(AssetSourcesUrdfPlugin {
-            assets_folder_local_path: "../../assets".to_owned(),
-        })
-        // default stuff bevy needs to run.
         .add_plugins(DefaultPlugins)
-        // plugins for robot editor
-        .add_plugins(RobotEditorPlugin)
-        // // serialization plugins
-        .add_plugins(SerializationPlugin)
-        .add_plugins(PhysicsSerializationPlugin)
-        .add_plugins(UrdfSerializationPlugin)
-        // physics
-        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
-        .add_plugins(RapierDebugRenderPlugin::default())
-        // world setup
-        //.add_systems(Update, visualize_window_for::<GizmoFocused>)
+        .add_plugins(WorldInspectorPlugin::default())
         .add_systems(Startup, setup)
-        .add_systems(PostStartup, turn_on_editor)
-        .add_systems(Update, debug_mouse_info)
+        .add_systems(Update, health_ui)
         .run();
 }
 
-fn turn_on_editor(mut commands: Commands) {
-    commands.insert_resource(NextState(Some(RobotEditorState::Active)));
-}
-
+/// set up a simple 3D scene
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    mut urdf_load_requests: ResMut<AssetSpawnRequestQueue<Urdf>>,
 ) {
-    // robot
-    urdf_load_requests.requests.push_front(AssetSpawnRequest {
-        source: format!("{:#}://model_pkg/urdf/diff_bot.xml", ROOT)
-            .to_owned()
-            .into(),
-        position: Transform::from_xyz(0.0, 15.0, 0.0),
-        ..Default::default()
+    // circular base
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Circle::new(4.0)),
+        material: materials.add(Color::WHITE),
+        transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+        ..default()
     });
-
-    // plane
-    commands.spawn((
+    // cube
+    commands.spawn(
+        (
         PbrBundle {
-            mesh: meshes.add(
-                Plane3d::new(Vec3::new(0.0, 1.0, 0.0))
-                    .mesh()
-                    .size(50.0, 50.0),
-            ),
-            material: materials.add(Color::rgb(0.3, 0.5, 0.3)),
-            transform: Transform::from_xyz(0.0, -1.0, 0.0),
+            mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+            material: materials.add(Color::rgb_u8(124, 144, 255)),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
             ..default()
         },
-        PhysicsBundle::default(),
-    ));
-
+        Health::default()
+    )
+);
     // light
-    commands.spawn((PointLightBundle {
+    commands.spawn(PointLightBundle {
         point_light: PointLight {
-            intensity: 1500.0,
             shadows_enabled: true,
             ..default()
         },
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..default()
-    },));
-    //camera
-    commands.spawn((
-        Camera3dBundle {
-            transform: Transform::from_xyz(2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-            ..Default::default()
-        },
-        //bevy_transform_gizmo::GizmoPickSource::default(),
-    ));
+    });
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
---
+++ crates/combat/src/components.rs
@@ -0,0 +1,19 @@
+use bevy::{ecs::component::Component, prelude::{Deref, DerefMut}};
+
+
+/// Health points.
+#[derive(Component)]
+pub struct Health {
+    pub hp: i32,
+    pub max_hp: i32,
+
+}
+
+impl Default for Health {
+    fn default() -> Self {
+        Self {
+            hp: 100,
+            max_hp: 100,
+        }
+    }
+}
