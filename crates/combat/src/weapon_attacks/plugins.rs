c0d91be445c707c63299cf1f2cd0322dbe997b02
TheMallus <162246706+TheMallus@users.noreply.github.com>
2024-11-20 10:09:00 -0600

    Added fire rate variable

--- crates/combat/src/weapon_attacks/plugins.rs
+++ crates/combat/src/weapon_attacks/plugins.rs
@@ -2,9 +2,13 @@ use bevy::prelude::*;
 use super::fire_shot::*;
 
 pub struct BulletPlugin;
-
+pub const SPAWN_TIME_SECONDS: f32 = 0.1;
+//Update constant to change bullet fire rate
 impl Plugin for BulletPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, fire_bullet);
+        app.insert_resource(SpawnTimer {
+            timer: Timer::from_seconds(SPAWN_TIME_SECONDS, TimerMode:: Repeating)
+        })
+            .add_systems(Update, fire_bullet);
     }
 }
