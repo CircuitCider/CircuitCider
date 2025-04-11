5acd6e16c37a1cb57b3a74dce3269b79b099ea67
Abel <alostaunauwave@gmail.com>
2025-04-10 19:20:50 -0500

    minor changes

--- crates/combat/src/weapons/systems.rs
+++ crates/combat/src/weapons/systems.rs
@@ -4,7 +4,7 @@ use bevy::prelude::*;
 pub fn apply_collision_damage(
     collision_damage_query: Query<(Entity, &CollisionDamage)>,
     health_query: Query<&mut Health>,
-    //rapier_context: Res<RapierContext>,
+    // rapier_context: Res<RapierContext>,
     name_query: Query<&Bullet>,
     commands: Commands,
 ) {
@@ -23,7 +23,7 @@ pub fn apply_collision_damage(
     //             }
     //         }
     //     }
-}
+} // commented out till further notice
 pub fn update_position(mut query: Query<(&Velocity, &mut Transform)>, time: Res<Time>) {
     for (velocity, mut transform) in query.iter_mut() {
         transform.translation += velocity.value * time.delta_secs();
